fad3c75fb40b0a92b0746f6e7c513f39b77ef32d
MustaGitRefresh <kabcoco.2gitdom.3@gmail.com>
2023-06-16 19:38:47 +0530

    Now this can read any file by just giving the name. Now follow th e video.

--- Jarvis_AI/src/Jarvis_AI.py
+++ Jarvis_AI/src/Jarvis_AI.py
@@ -1,43 +1,47 @@
 # IMPORTS
 import speaker_output
 import speech_recognition as sr
-import Reader as reader
+import Reader
 
 
 # The Code Starts Here
 class Jarvis:
 
     def __init__(self):
-        self.query = None
+        self.file_name = None
         speaker_output.say("Hello! I am listening to you")
         while True:
             print("listening")
-            self.takeCommand()
+            self.query = self.takeCommand()
             if self.query is not None:
-                speaker_output.say(self.query)
+                speaker_output.say("Running your command")
+                self.checkCommand()
             else:
                 speaker_output.say("Please repeat it again")
 
-    def takeCommand(self):
+    def takeCommand(self, prompt=""):
         mic = sr.Recognizer()
         with sr.Microphone() as source:
             mic.pause_threshold = 0.8
             mic.energy_threshold = 270
             audio = mic.listen(source)
             try:
-                text = mic.recognize_google(audio, language='hi-in')
+                if prompt:
+                    speaker_output.say(prompt)
+                text = mic.recognize_google(audio, language='en-in')
                 print(f"User said {text}")
-                self.query = text
+                return str(text)
             except sr.UnknownValueError:
                 print("Text is not recognisable")
                 self.query = None
                 return
 
-    # todo: Modify it on some occasion.
-    def reader(self):
-        """This is the reader"""
-        text = reader.reader('speaker_output.py')
-        speaker_output.say(str(text))
+    def checkCommand(self):
+        if "read file" in self.query.lower():
+            speaker_output.say("Please enter the file name")
+            self.file_name = input()
+            file_text = Reader.reader(self.file_name)
+            speaker_output.say(str(file_text))
 
 
 jarvis = Jarvis()
